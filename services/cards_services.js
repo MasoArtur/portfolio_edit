caaefc1f3bc044c6971d3bd388bb8360189f14f4
Artur Maso <masoartur@gmail.com>
2025-05-21 14:33:59 -0300

    aaa

---
+++ services/cards_services.js
@@ -0,0 +1,10 @@
+import { bd_cards_comida } from "./../view/js/create_cards.js";
+
+function createCardsComida(){
+    let cards_comida = document.getElementById('cards');
+
+    for(let i = 0; i > nome.length; i++){
+
+    }
+
+}
---
+++ view/js/create_cards.js
@@ -0,0 +1,35 @@
+export const bd_cards_comida = [
+    {
+        nome: "Brigadeiro",
+        tipo: "doce",
+    },
+    {
+        nome: "Pudim",
+        tipo: "doce",
+    },
+    {
+        nome: "Coxinha",
+        tipo: "salgado",
+    },
+    {
+        nome: "Empada",
+        tipo: "salgado",
+
+    },
+    {
+        nome: "Torta de Morango",
+        tipo: "doce",
+    },
+    {
+        nome: "Pastel",
+        tipo: "salgado",
+    },
+    {
+        nome: "palha-italiana",
+        tipo: "doce",
+    },
+    {
+        nome: "bauru",
+        tipo: "salgado",
+    }
+];
